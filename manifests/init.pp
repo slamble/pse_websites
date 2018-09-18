63d034092d5d7e963514a27fd1b1f10da4fc7023
Stuart Lamble <slamble@diaxion.com>
2018-09-18 16:26:29 +1000

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+# websites: manages five Apache websites using the Puppetlabs Apache module.
+#
+# @summary For the PSE evaluation.
+#
+# @example
+#   include websites
+class websites {
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  $docroot = '/var/www/vhost'
+  vhosts = [ 'vhost1', 'vhost2', 'vhost3', 'vhost4', 'vhost5' ]
+  $vhosts.each |Integer $index, String $vhost| {
+    apache::vhost { $vhost:
+      port          => 8000 + $index,
+      docroot       => "${docroot}/${vhost}",
+      docroot_owner => 'www-data',
+      docroot_group => 'www-data',
+    }
+    file { "${docroot}/${vhost}/index.html":
+      ensure => present,
+      owner => 'www-data',
+      group => 'www-data',
+      content => "<HTML><HEAD><TITLE>Test website ${vhost}</TITLE></HEAD><BODY>Just a test website for vhost ${vhost}.</BODY></HTML>",
+    }
+  }
+}
